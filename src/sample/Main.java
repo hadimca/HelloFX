78e3a8b70b66c569c85588ffc3633e760c3a2a81
hadim <Tehran12>
2022-11-15 13:59:17 +0330

    First Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 310, 275));
+        primaryStage.setScene(new Scene(root, 320, 275));
         primaryStage.show();
     }
 
